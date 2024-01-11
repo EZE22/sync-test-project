e1d88844411bc361823cd867ba12594e91733c03
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2024-01-11 16:54:15 -0500

    Variable Test

--- src/java_code/Main.java
+++ src/java_code/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        for(int i = 1; i > 0; i++) {
-            System.out.println("what");
-        }
+        int n;
+
     }
 
 }
