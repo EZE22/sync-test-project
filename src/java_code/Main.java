1982254673e135bb0d796a9c0b65821af173e63c
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2024-01-11 16:49:50 -0500

    Infinite Loop Test

--- src/java_code/Main.java
+++ src/java_code/Main.java
@@ -3,5 +3,10 @@ package java_code;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for(int i = 1; i > 0; i++) {
+            System.out.println("what");
+        }
     }
+
 }
