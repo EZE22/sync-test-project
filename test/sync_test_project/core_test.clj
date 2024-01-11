0e7f5c1fedc1bb58598cb87793a0c1c87a1c9d32
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2024-01-11 16:08:45 -0500

    Project Creation

---
+++ test/sync_test_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns sync-test-project.core-test
+  (:require [clojure.test :refer :all]
+            [sync-test-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
